63995a674177d4cc0ba97cb0c57ce2d344ca868b
bikal98 <Vpham3218@gmail.com>
2019-07-11 12:19:35 +0700

    lam chuc nang hien thi danh sach dang nhan do

--- src/app/page/listgiveget/listgiveget.page.ts
+++ src/app/page/listgiveget/listgiveget.page.ts
@@ -14,12 +14,23 @@ export class ListgivegetPage implements OnInit {
 
   products : any;
   userID : any;
+  userSubmit : any;
   constructor(
     public alertController: AlertController,
     private crudProduct: CrudProduct,
     private authService: AuthService
   ){}
 
+   //lay thong tin nguoi dung da dang ky 
+  getUserHasSubmit(productID){
+    this.crudProduct.read_GetProduct(productID).subscribe(data => {
+      this.userSubmit = data.map(e => {
+        return {
+          user:e.payload.doc.data()['user']
+        };
+      })
+    });
+  }
   ngOnInit() {
     this.crudProduct.read_Products().subscribe(data => {
       this.products = data.map(e => {
@@ -40,7 +51,11 @@ export class ListgivegetPage implements OnInit {
       this.userID = this.authService.userDetails().uid;
     }
   }
-
+  ionViewDidEnter(){
+    for(var i =0;i<this.products.length;i++){
+      this.getUserHasSubmit(this.products[i].id);
+    }
+  }
   async segmentChanged() {
     await this.slider.slideTo(this.segment);
   }
